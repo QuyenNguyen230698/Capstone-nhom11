9e260cfdff11bf1835212b1abaa1ed058d1bb6d3
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2024-06-20 21:17:38 +0700

    update

--- Quyen-nhom11/JS/Quyen.js
+++ Quyen-nhom11/JS/Quyen.js
@@ -6,4 +6,6 @@ document.getElementById("switchButton").onclick = function () {
   // Theme Change
 
 
-  
+  var offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
+  offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+  scrollDuration = 700;
